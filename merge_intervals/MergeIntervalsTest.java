d8383264775a8a887df29773f9979cb2501d7b96
Ardavan Ghaffari <ghaffari.ardavan@gmail.com>
2024-11-12 14:57:57 +0100

    Reorganize imports

--- merge_intervals/MergeIntervalsTest.java
+++ merge_intervals/MergeIntervalsTest.java
@@ -1,10 +1,10 @@
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
 import static org.junit.Assert.assertArrayEquals;
 
 import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class MergeIntervalsTest {
 
