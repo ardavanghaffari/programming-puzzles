c027cd7a7a1a8eb4167746087f522b2d410f34cf
Ardavan Ghaffari <ghaffari.ardavan@gmail.com>
2025-01-01 14:23:56 +0100

    Enable usage of third party libs in Java code

--- run_junit_test.sh
+++ run_junit_test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
 mkdir -p target
-javac -d target -cp ${PROJECT_ROOT}/lib/junit-platform-console-standalone-1.11.0.jar ${1}.java ${1}Test.java
-java -jar ${PROJECT_ROOT}/lib/junit-platform-console-standalone-1.11.0.jar execute \
-    --class-path target \
-    --select-class ${1}Test
+javac -Xdiags:verbose -d target -cp "${PROJECT_ROOT}/lib/*" ${1}.java ${1}Test.java
+java -cp "${PROJECT_ROOT}/lib/*" org.junit.platform.console.ConsoleLauncher execute --class-path target --select-class ${1}Test
